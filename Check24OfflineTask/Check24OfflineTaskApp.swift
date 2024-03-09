3601b745c4ab923642c9581951c4a2f93c19d905
Ali Görkem Aksöz <a.gorkemaksoz@gmail.com>
2024-03-09 12:21:25 +0300

    Initial Commit

---
+++ Check24OfflineTask/Check24OfflineTaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  Check24OfflineTaskApp.swift
+//  Check24OfflineTask
+//
+//  Created by Ali Görkem Aksöz on 9.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct Check24OfflineTaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
